30e3cfa376fbbe29af0369209f1413a92aafd90a
mhm8824-alt <mhm8824@gmail.com>
2026-01-14 00:03:49 +0300

    fix AppRegistryNotReady error

--- reports/apps.py
+++ reports/apps.py
@@ -3,3 +3,24 @@ from django.apps import AppConfig
 
 class ReportsConfig(AppConfig):
     name = 'reports'
+
+
+
+
+
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+class CoreConfig(AppConfig): # تأكد أن الاسم يطابق الموجود عندك
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core' # تأكد أن هذا هو اسم تطبيقك
+
+    def ready(self):
+        # استدعاء الدالة عند اكتمال التحميل
+        post_migrate.connect(create_superuser, sender=self)
+
+def create_superuser(sender, **kwargs):
+    from django.contrib.auth.models import User # الاستيراد هنا آمن
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+        print("Done: Admin created successfully!")
